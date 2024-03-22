a7e8f1207363474e153251b77d698ad9760c92bd
Aruna Sankaranarayanan <arunas@align-3.csail.mit.edu>
2024-03-22 18:05:14 -0400

    new prompt prologue, set consistent random seed

--- icl-notebooks/icl_grammars.py
+++ icl-notebooks/icl_grammars.py
@@ -16,6 +16,10 @@ import os
 import pickle
 import yaml
 import argparse
+
+random.seed(42)
+torch.manual_seed(42)
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--config', type=str, help='path to the model training config file, found in broca/configs')
@@ -30,14 +34,10 @@ PREFIX = config.prefix
 MODEL_NAME = config.model_name
 MODEL_PATH = config.model_path
 ABLATION = config.ablation
-ABLATION_PICKLE_PREFIX = config.ablation_prefix
-ABLATION_PICKLE_SUFFIX = config.ablation_suffix
-MEAN_ABLATION = config.mean_ablation
 DATA_PATH = config.data_path
 NUM_DEMONSTRATIONS = config.num_demonstrations
 BATCH_SIZE = config.batch_size
-FINAL_CSV_PREFIX = config.final_csv_prefix
-FINAL_CSV_SUFFIX = config.final_csv_suffix
+FINAL_CSV_PATH = config.final_csv_path
 
 nf4_config = BitsAndBytesConfig(
     load_in_4bit=True,
@@ -57,8 +57,10 @@ df = pd.read_csv(f'{DATA_PATH}')
 gCols = [col for col in list(df.columns) if not 'ng' in col]
 col = gCols[int(sys.argv[1])]
 
-
 if (ABLATION):
+    MEAN_ABLATION = config.mean_ablation
+    ABLATION_PICKLE_PREFIX = config.ablation_prefix
+    ABLATION_PICKLE_SUFFIX = config.ablation_suffix
     def ablate_model(col):
         with open(f'{PREFIX}/broca/{MODEL_NAME}/{MODEL_NAME}-attr-patch-scripts/mlp/{ABLATION_PICKLE_PREFIX}-{col}-{ABLATION_PICKLE_SUFFIX}.pkl', 'rb') as f:
             print('ablating mlp ', col)
@@ -89,7 +91,7 @@ def parse_answer(text):
 def construct_prompt(train_dataset, num_demonstrations):
     assert num_demonstrations > 0
     prompt = ''
-    train_examples = train_dataset.shuffle().select(range(num_demonstrations))
+    train_examples = train_dataset.shuffle(seed=42).select(range(num_demonstrations))
     for exemplar_num in range(num_demonstrations):
         train_example = train_examples[exemplar_num]
         use_bad_sentence = random.choice([True, False])
@@ -177,7 +179,7 @@ f = pd.DataFrame(columns=["type", "prompt", "q", "prediction", "gold", "surprisa
 f['type'] = 'test'
 g = pd.DataFrame(columns=['accuracy', 'type'])
 datasets = {}
-datasets[col] = Dataset.from_pandas(pd.DataFrame(df[[col, 'ng-' + col]].copy())).train_test_split(test_size=0.5)
+datasets[col] = Dataset.from_pandas(pd.DataFrame(df[[col, 'ng-' + col]].copy())).train_test_split(test_size=0.5, random_state=42)
 
 def get_master_prompt(lang):
     en_verbs = ["affirms", "bring", "brings", "carries", "carry", "climb", "climbs", "eat", "eats", "hold", "holds", "knows", "notices", "read", "reads", "says", "sees", "take", "takes"]
@@ -214,55 +216,60 @@ def get_master_prompt(lang):
 
     if 'en' == "".join(lang[:2]):
         intro = "We will give you examples of English sentences that follow or violate the rules of a shared grammar, along with labels 'Yes' or 'No'. You will then generate a label, 'Yes' or 'No', for a new unlabeled sentence that may follow or violate the same grammar rules."
-        verbs = f"""The sentences may use verbs ({', '.join(en_verbs)});"""
-        pastTenseVerbs = f"""or their corresponding past tense forms ({', '.join(en_verbs_past)});""" 
-        infinitiveVerbs = f"""infinitive forms ({', '.join(en_verbs_infinitive)});"""
-        passiveVerbs = f"""or passive forms ({', '.join(en_verbs_passive)})."""
-        nouns = f"""The sentences may use nouns ({', '.join(set(en_nouns + en_nouns_plural))}) for the subjects and objects."""
-        properNouns = f"""The sentences may use proper nouns ({', '.join(set(proper_nouns))})."""
-        return f"""1.{intro}\n2.{verbs} {pastTenseVerbs} {infinitiveVerbs} {passiveVerbs}\n3.{nouns}\n4.{properNouns}"""
+        # verbs = f"""The sentences may use verbs ({', '.join(en_verbs)});"""
+        # pastTenseVerbs = f"""or their corresponding past tense forms ({', '.join(en_verbs_past)});""" 
+        # infinitiveVerbs = f"""infinitive forms ({', '.join(en_verbs_infinitive)});"""
+        # passiveVerbs = f"""or passive forms ({', '.join(en_verbs_passive)})."""
+        # nouns = f"""The sentences may use nouns ({', '.join(set(en_nouns + en_nouns_plural))}) for the subjects and objects."""
+        # properNouns = f"""The sentences may use proper nouns ({', '.join(set(proper_nouns))})."""
+        # return f"""1.{intro}\n2.{verbs} {pastTenseVerbs} {infinitiveVerbs} {passiveVerbs}\n3.{nouns}\n4.{properNouns}"""
+        return f"{intro}"
 
     elif 'it' == "".join(lang[:2]) and (len(lang) <=2 or lang[2] != 'a'):
         intro = "We will give you examples of English sentences stylized to Italian syntax that follow or violate the rules of a shared grammar, along with labels 'Yes' or 'No'. You will then generate a label, 'Yes' or 'No', for a new unlabeled sentence that may follow or violate the same grammar rules."
-        verbs = f"""The sentences may use verbs ({', '.join(en_verbs)});"""
-        pastTenseVerbs = f"""or their corresponding past tense forms ({', '.join(en_verbs_past)});""" 
-        infinitiveVerbs = f"""infinitive forms ({', '.join(en_verbs_infinitive)});"""
-        passiveVerbs = f"""or passive forms ({', '.join(en_verbs_passive)})."""
-        nouns = f"""The sentences may use nouns ({', '.join(set(en_nouns + en_nouns_plural))}) for the subjects and objects."""
-        properNouns = f"""The sentences may use proper nouns ({', '.join(set(proper_nouns))})."""
+        # verbs = f"""The sentences may use verbs ({', '.join(en_verbs)});"""
+        # pastTenseVerbs = f"""or their corresponding past tense forms ({', '.join(en_verbs_past)});""" 
+        # infinitiveVerbs = f"""infinitive forms ({', '.join(en_verbs_infinitive)});"""
+        # passiveVerbs = f"""or passive forms ({', '.join(en_verbs_passive)})."""
+        # nouns = f"""The sentences may use nouns ({', '.join(set(en_nouns + en_nouns_plural))}) for the subjects and objects."""
+        # properNouns = f"""The sentences may use proper nouns ({', '.join(set(proper_nouns))})."""
         gendered = f"""The nouns in the sentences have specific gender determiners - 'kar' (used by {', '.join(set(it_nouns_kar))}); 'kon' (used by {', '.join(set(it_nouns_kon))}); 'kars' (used by {', '.join(set(it_nouns_kars))}); 'kons' (used by {', '.join(set(it_nouns_kons))})."""
-        return f"""1.{intro}\n2.{verbs} {pastTenseVerbs} {infinitiveVerbs} {passiveVerbs}\n3.{nouns}\n4.{properNouns}\n5.{gendered}"""
+        # return f"""1.{intro}\n2.{verbs} {pastTenseVerbs} {infinitiveVerbs} {passiveVerbs}\n3.{nouns}\n4.{properNouns}\n5.{gendered}"""
+        return f"1. {intro}\n2. {gendered}"
     
     elif 'ita' == "".join(lang[:3]):
         intro = "We will give you examples of Italian sentences that follow or violate the rules of a shared grammar, along with labels 'Yes' or 'No'. You will then generate a label, 'Yes' or 'No', for a new unlabeled sentence that may follow or violate the same grammar rules."
-        verbs = f"""The sentences may use verbs ({', '.join(ita_verbs)});"""
-        pastTenseVerbs = f"""or their corresponding past tense forms ({', '.join(ita_verbs_past)});""" 
-        infinitiveVerbs = f"""infinitive forms ({', '.join(ita_verbs_infinitive)});"""
-        passiveVerbs = f"""or passive forms ({', '.join(ita_verbs_past)})."""
-        nouns = f"""The sentences may use nouns ({', '.join(set(ita_nouns + ita_nouns_plurals))}) for the subjects and objects."""
-        properNouns = f"""The sentences may use proper nouns ({', '.join(set(proper_nouns))})."""
-        gendered = f"""The nouns in the sentences have specific gender determiners - 'la' (used by {', '.join(set(ita_nouns_la))}); 'lo' (used by {', '.join(set(ita_nouns_lo))}); 'le' (used by {', '.join(set(ita_nouns_le))}); 'il' (used by {', '.join(set(ita_nouns_il))}, "l'" (used by {', '.join(set(ita_nouns_il_vowel))}); 'i' (used by {', '.join(set(ita_nouns_i))}, and 'gli' (used by {', '.join(set(ita_nouns_gli))}."""
-        return f"""1.{intro}\n2.{verbs} {pastTenseVerbs} {infinitiveVerbs} {passiveVerbs}\n3.{nouns}\n4.{properNouns}\n5.{gendered}"""
+        # verbs = f"""The sentences may use verbs ({', '.join(ita_verbs)});"""
+        # pastTenseVerbs = f"""or their corresponding past tense forms ({', '.join(ita_verbs_past)});""" 
+        # infinitiveVerbs = f"""infinitive forms ({', '.join(ita_verbs_infinitive)});"""
+        # passiveVerbs = f"""or passive forms ({', '.join(ita_verbs_past)})."""
+        # nouns = f"""The sentences may use nouns ({', '.join(set(ita_nouns + ita_nouns_plurals))}) for the subjects and objects."""
+        # properNouns = f"""The sentences may use proper nouns ({', '.join(set(proper_nouns))})."""
+        # gendered = f"""The nouns in the sentences have specific gender determiners - 'la' (used by {', '.join(set(ita_nouns_la))}); 'lo' (used by {', '.join(set(ita_nouns_lo))}); 'le' (used by {', '.join(set(ita_nouns_le))}); 'il' (used by {', '.join(set(ita_nouns_il))}, "l'" (used by {', '.join(set(ita_nouns_il_vowel))}); 'i' (used by {', '.join(set(ita_nouns_i))}, and 'gli' (used by {', '.join(set(ita_nouns_gli))}."""
+        # return f"""1.{intro}\n2.{verbs} {pastTenseVerbs} {infinitiveVerbs} {passiveVerbs}\n3.{nouns}\n4.{properNouns}\n5.{gendered}"""
+        return f"{intro}"
     
     elif 'jap' == "".join(lang[:3]):
         intro = "We will give you examples of Japanese sentences that follow or violate the rules of a shared grammar, along with labels 'Yes' or 'No'. You will then generate a label, 'Yes' or 'No', for a new unlabeled sentence that may follow or violate the same grammar rules."
-        verbs = f"""The sentences may use verbs ({', '.join(jp_verbs)});"""
-        passiveVerbs = f"""or passive forms ({', '.join(jp_verbs_passive)})."""
-        nouns = f"""The sentences may use nouns ({', '.join(set(jp_nouns))}) for the subjects and objects."""
-        suffixes = f"""The sentences may use suffixes ({', '.join(set(jp_suffixes))}) along with subjects, objects or verbs."""
-        properNouns = f"""The sentences may use proper nouns ({', '.join(set(jp_proper_nouns))})."""
-        return f"""1.{intro}\n2.{verbs} {passiveVerbs}\n3.{nouns}\n4.{properNouns}\n5.{suffixes}"""
+        # verbs = f"""The sentences may use verbs ({', '.join(jp_verbs)});"""
+        # passiveVerbs = f"""or passive forms ({', '.join(jp_verbs_passive)})."""
+        # nouns = f"""The sentences may use nouns ({', '.join(set(jp_nouns))}) for the subjects and objects."""
+        # suffixes = f"""The sentences may use suffixes ({', '.join(set(jp_suffixes))}) along with subjects, objects or verbs."""
+        # properNouns = f"""The sentences may use proper nouns ({', '.join(set(jp_proper_nouns))})."""
+        # return f"""1.{intro}\n2.{verbs} {passiveVerbs}\n3.{nouns}\n4.{properNouns}\n5.{suffixes}"""
+        return f"{intro}"
     
     elif 'jp' == "".join(lang[:2]):
         intro = "We will give you examples of English sentences stylized to Japanese syntax that follow or violate the rules of a shared grammar, along with labels 'Yes' or 'No'. You will then generate a label, 'Yes' or 'No', for a new unlabeled sentence that may follow or violate the same grammar rules."
-        verbs = f"""The sentences may use verbs ({', '.join(en_verbs)});"""
-        pastTenseVerbs = f"""or their corresponding past tense forms ({', '.join(en_verbs_past)});""" 
-        infinitiveVerbs = f"""infinitive forms ({', '.join(en_verbs_infinitive)});"""
-        passiveVerbs = f"""or passive forms ({', '.join(en_verbs_passive)});"""
-        nouns = f"""The sentences may use nouns ({', '.join(set(en_nouns + en_nouns_plural))}) for the subjects and objects."""
-        properNouns = f"""The sentences may use proper nouns ({', '.join(set(proper_nouns))})."""
-        suffixes = f"""The sentences use Japanese suffixes such as wa (commonly used after the subject); o, ni, ga, o-ta (commonly used after the object); reru(used after the verb)."""
-        return f"""1.{intro}\n2.{verbs} {pastTenseVerbs} {infinitiveVerbs} {passiveVerbs}\n3.{nouns}\n4.{properNouns}\n5.{suffixes}"""
+        # verbs = f"""The sentences may use verbs ({', '.join(en_verbs)});"""
+        # pastTenseVerbs = f"""or their corresponding past tense forms ({', '.join(en_verbs_past)});""" 
+        # infinitiveVerbs = f"""infinitive forms ({', '.join(en_verbs_infinitive)});"""
+        # passiveVerbs = f"""or passive forms ({', '.join(en_verbs_passive)});"""
+        # nouns = f"""The sentences may use nouns ({', '.join(set(en_nouns + en_nouns_plural))}) for the subjects and objects."""
+        # properNouns = f"""The sentences may use proper nouns ({', '.join(set(proper_nouns))})."""
+        suffixes = f"""The sentences use Japanese topic markers and suffixes such as wa (commonly used after the subject); o, ni, ga, o-ta (commonly used after the object); reru(used after the verb)."""
+        # return f"""1.{intro}\n2.{verbs} {pastTenseVerbs} {infinitiveVerbs} {passiveVerbs}\n3.{nouns}\n4.{properNouns}\n5.{suffixes}"""
+        return f"1. {intro}\n2. {suffixes}"
 
 master_prompt = get_master_prompt(col)
 train_dataset = datasets[col]['train']
@@ -270,6 +277,8 @@ test_dataset = datasets[col]['test']
 printAnswer = False
 f = pd.DataFrame(columns=["type", "prompt", "q", "prediction", "gold", "surprisal", "int-grad"])
 for i in range(0, len(test_dataset), BATCH_SIZE):
+    if (i > 10):
+        break
     test_sentences = []
     fPrompts = []
     fQs = []
@@ -316,10 +325,10 @@ for i in range(0, len(test_dataset), BATCH_SIZE):
     fPredictions = parse_answer(answers)
     fSurprisals = get_aligned_words_measures(test_sentences, parse_answer(answers), "surp", model, tokenizer)
     for batch_idx in range(len(fPrompts)):
-        f = pd.concat([f, pd.DataFrame([{'type': col, 'prompt': fPrompts[batch_idx], 'q' :test_sentences[batch_idx], 'prediction': fPredictions[batch_idx], 'gold': fGolds[batch_idx], 'surprisal': fSurprisals[batch_idx], 'int-grad': 0}])]).reset_index(drop=True)
+        f = pd.concat([f, pd.DataFrame([{'type': col, 'prompt': prompts[batch_idx], 'q' :test_sentences[batch_idx], 'prediction': fPredictions[batch_idx], 'gold': fGolds[batch_idx], 'surprisal': fSurprisals[batch_idx], 'int-grad': 0}])]).reset_index(drop=True)
 # Evaluate
 accuracy = compute_accuracy(preds, golds)
 print(f"{col} -- Accuracy: {accuracy:.2f}\n")
 g = pd.concat([g, pd.DataFrame([{ 'trainType' : col, 'testType': col, 'accuracy': f"{accuracy:.2f}"}])])
-f.to_csv(f"{PREFIX}/broca/{MODEL_NAME}/experiments/{MODEL_NAME}-{FINAL_CSV_PREFIX}-{col}-{FINAL_CSV_SUFFIX}.csv")
-g.to_csv(f'{PREFIX}/broca/{MODEL_NAME}/experiments/{MODEL_NAME}-{FINAL_CSV_PREFIX}-{col}-{FINAL_CSV_SUFFIX}.csv')
+f.to_csv(f"{PREFIX}/broca/{MODEL_NAME}/experiments/{col}.csv")
+g.to_csv(f'{PREFIX}/broca/{MODEL_NAME}/experiments/{col}.csv')
