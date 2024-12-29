ed210f512f3984a0fb657518bb9ab31b142d560e
Aruna Sankaranarayanan <arunas@align-4.csail.mit.edu>
2024-12-29 13:42:15 -0500

    data-gen and configs

---
+++ data-gen/rename-new-file-format.sh
@@ -0,0 +1,22 @@
+mv jap-r-3-subordinate-acc.csv jap-r-2-subordinate-acc.csv
+mv jap-r-3-subordinate.csv jap-r-2-subordinate.csv
+mv en-acc.csv en-r-1-acc.csv
+mv en-u-4-wh-acc.csv en-u-3-wh-acc.csv
+mv ita-u-2-invert-acc.csv ita-u-2-inversion-acc.csv
+mv en.csv en-r-1.csv
+mv en-u-4-wh.csv en-u-3-wh.csv
+mv ita-u-2-invert.csv ita-u-2-inversion.csv
+mv en-r-1-subordinate-acc.csv en-r-2-subordinate-acc.csv
+mv ita-acc.csv ita-r-1-acc.csv
+mv ita-u-3-gender-acc.csv
+mv jap-u-2-invert-acc.csv jap-u-2-inversion-acc.csv
+mv en-r-1-subordinate.csv en-r-2-subordinate.csv
+mv ita.csv ita-r-1.csv
+mv jap-u-2-invert.csv jap-u-2-inversion.csv
+mv en-r-2-passive-acc.csv en-r-3-passive-acc.csv
+mv jap-r-1-sov-acc.csv jap-r-1-acc.csv
+mv en-r-2-passive.csv en-r-3-passive.csv
+mv jap-r-1-sov.csv jap-r-1.csv
+mv jap-r-2-passive-acc.csv jap-r-3-passive-acc.csv
+mv jap-r-2-passive.csv jap-r-3-passive.csv
+
